c903d20429dd55a52aac0169059d6f49b74f07f4
Santhosh <santoshmv121@gmail.com>
2021-02-11 00:15:24 +0530

    Initial Commit

---
+++ src/main/java/com/santhosh/vernekar/gatewayservice/filter/AddRequestHeaderFilter.java
@@ -0,0 +1,33 @@
+package com.santhosh.vernekar.gatewayservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+
+
+public class AddRequestHeaderFilter extends ZuulFilter {
+
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx= RequestContext.getCurrentContext();
+        ctx.addZuulRequestHeader("x-location","India");
+        return null;
+    }
+}
